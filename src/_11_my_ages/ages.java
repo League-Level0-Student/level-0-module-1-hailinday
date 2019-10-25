6c7f30417be9d0f883b200453a9b9a6265535866
League Student <league@iMac-9.attlocal.net>
2019-10-25 16:55:31 -0700

    Badger

--- src/_11_my_ages/ages.java
+++ src/_11_my_ages/ages.java
@@ -4,9 +4,10 @@ import javax.swing.JOptionPane;
 
 public class ages {
 public static void main(String[] args) {
-	for (int i = 2005; i <2019; i++) {
+	for (int i = 2005; i <2020; i++) {
 		
 		System.out.println(i);
+		
 	}
 }
 }
---
+++ src/_12_badgers/badger.java
@@ -0,0 +1,21 @@
+package _12_badgers;
+
+import javax.swing.JOptionPane;
+
+public class badger {
+public static void main(String[] args) {
+for (int i = 0; i < 15; i++) {
+	
+
+	for (int h = 0; h < 12; h++) {
+		JOptionPane.showMessageDialog(null, "badger");	
+	}	
+	for (int j = 0; j < 2; j++) {
+		JOptionPane.showMessageDialog(null, "mushroom");	
+	}
+	for (int j = 0; j < 5; j++) {
+		JOptionPane.showMessageDialog(null, "snake");
+}
+}
+}
+}
